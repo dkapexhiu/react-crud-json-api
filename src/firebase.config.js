802afedf550563f0c049a4c89550522f028e5654
Daniel <daniel.kapexhiu@gmail.com>
2020-03-12 20:20:33 +0100

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBz4bWcC_h6rKV7Mq5bG6z2tN5zEWJ5U2Y",
-  authDomain: "scrim-58b33.firebaseapp.com",
-  databaseURL: "https://scrim-58b33.firebaseio.com",
-  projectId: "scrim-58b33",
-  storageBucket: "scrim-58b33.appspot.com",
-  messagingSenderId: "108163156657",
-  appId: "1:108163156657:web:a0f780a0afe1543b303090"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
   };
 
 export default firebase.initializeApp(config);
